4266f2bae7d0a10d1a16773a3ed1b413dd3139f1
Its-Delimas <spencerdelimas@gmail.com>
2025-11-14 13:57:23 +0300

    ""

--- alpha/src/App.jsx
+++ alpha/src/App.jsx
@@ -1,37 +1,38 @@
-import { useState } from "react";
-import './App.css'
+import { useState } from 'react';
 
-export default function main (){
+export default function MyForm() {
+  const [inputs, setInputs] = useState({ firstname: "", lastname: "" });
 
-  const [inputs,setInputs]=useState({});
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({ ...values, [name]: value }));
+  }
 
-    const handlechange = (e) =>{
-          const name=e.target.name;
-          const value=e.target.value;
-          setInputs(values=>({...values,[name]:value}))
-    }
-
-
-    return (
-      <form>
-          <label>First name:
-            <input 
-              type="text"
-              name="firstname"
-              value={inputs.firstname}
-              onChange={handlechange}
-            />
-          </label>
-          <label>Last Name:
-              <input 
-                type="text"
-                name="lastname"
-                value={inputs.lastname}
-                onChange={handlechange}
-              />
-          </label>
-      <p>current values : {inputs.firstname} {inputs.lastname}</p>
-      </form>
-    )
-
+  return (
+    <form>
+      <label>
+        First name:
+        <input 
+          type="text" 
+          name="firstname" 
+          value={inputs.firstname} 
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Last name:
+        <input 
+          type="text" 
+          name="lastname" 
+          value={inputs.lastname} 
+          onChange={handleChange}
+        />
+      </label>
+      <p>Current values: {inputs.firstname} {inputs.lastname}</p>
+    </form>
+  );
 }
+
+
