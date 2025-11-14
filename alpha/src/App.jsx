9673e095a493c9588c20eca7cc2910c5000e7c79
Its-Delimas <spencerdelimas@gmail.com>
2025-11-14 13:54:46 +0300

    Controlled form

--- alpha/src/App.jsx
+++ alpha/src/App.jsx
@@ -2,19 +2,35 @@ import { useState } from "react";
 import './App.css'
 
 export default function main (){
-  const[myCar ,setMyCar]=useState("Volvo")
 
-    const handleChange = (event) =>{
-      setMyCar (event.target.value)
+  const [inputs,setInputs]=useState({});
+
+    const handlechange = (e) =>{
+          const name=e.target.name;
+          const value=e.target.value;
+          setInputs(values=>({...values,[name]:value}))
     }
+
 
     return (
       <form>
-        <select value={myCar} onChange={handleChange}>
-            <option value="Ford">Ford</option>
-            <option value="Volvo">Volvo</option>
-            <option value="Flat">Flat</option>
-        </select>
+          <label>First name:
+            <input 
+              type="text"
+              name="firstname"
+              value={inputs.firstname}
+              onChange={handlechange}
+            />
+          </label>
+          <label>Last Name:
+              <input 
+                type="text"
+                name="lastname"
+                value={inputs.lastname}
+                onChange={handlechange}
+              />
+          </label>
+      <p>current values : {inputs.firstname} {inputs.lastname}</p>
       </form>
     )
 
